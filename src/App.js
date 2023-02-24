beeaf331feec52dedaf91cfab75a58990eed3685
Nirmal Mekala <nirmal@clockwork.com>
2023-02-24 11:34:09 -0600

    manage currentUser in state

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import netlifyIdentity from 'netlify-identity-widget';
 
 function App() {
+  const [currentUser, setCurrentUser] = useState(null);
+  const user = netlifyIdentity.currentUser();
+
   useEffect(() => {
     netlifyIdentity.init();
-    console.log('hi');
+    netlifyIdentity.on('init', (user) => {
+      console.log('init', user);
+      setCurrentUser(user);
+    });
+    netlifyIdentity.on('login', (user) => {
+      console.log('login', user);
+      setCurrentUser(user);
+    });
+    netlifyIdentity.on('logout', () => {
+      console.log('Logged out', user);
+      setCurrentUser(user);
+    });
+    netlifyIdentity.on('error', (err) => console.error('Error', err));
+    netlifyIdentity.on('open', () => console.log('Widget opened'));
+    netlifyIdentity.on('close', () => console.log('Widget closed'));
+    // eslint-disable-next-line
   }, []);
 
   return (
@@ -32,6 +50,7 @@ function App() {
         >
           Login
         </button>
+        {currentUser && <p>state says logged in</p>}
       </header>
     </div>
   );
